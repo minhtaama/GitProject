dc43bc42410657920e025bec72872ee22f50ee74
minhtaama <ngminhtam6697@gmail.com>
2022-06-11 02:39:03 +0700

    new bouncing ball power

--- case-study/script.js
+++ case-study/script.js
@@ -1,18 +1,19 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
-class Ball {
-    constructor (radius) {
+class Bal {
+    constructor (radius,power) {
         this.radius = radius;
         this.x;
         this.y;
         this.xFlag = 0; //0: move left, 1: move right
         this.yFlag = 0; //0: move up, 1: move down
-        this.spHorizon = Math.sqrt(14.5);
-        this.spVertical = Math.sqrt(10.5);
+        this.spHorizon = Math.sqrt(8);
+        this.spVertical = Math.sqrt(8);
         this.touchBorderBottom = 0;
         this.xT;        //coordinates of nearest touch point on target
         this.yT;        //
+        this.isHasPower = power;
     }
     getCoordinates(x,y) {
         this.x = x;
@@ -37,17 +38,17 @@ class Ball {
         } else if (this.y > target.y  && this.y < target.y + target.height) {
             this.yT = this.y;
         }
-        ///drawRange///
-            // ctx.beginPath();
-            // ctx.arc(this.xT,this.yT,2,0,2*Math.PI);
-            // ctx.fillStyle = "blue";
-            // ctx.fill();
-            // ctx.beginPath();
-            // ctx.moveTo(this.x,this.y);
-            // ctx.lineTo(this.xT,this.yT);
-            // ctx.strokeStyle = "gray";
-            // ctx.stroke();
-        //////////////
+        // /drawRange///
+        //     ctx.beginPath();
+        //     ctx.arc(this.xT,this.yT,2,0,2*Math.PI);
+        //     ctx.fillStyle = "blue";
+        //     ctx.fill();
+        //     ctx.beginPath();
+        //     ctx.moveTo(this.x,this.y);
+        //     ctx.lineTo(this.xT,this.yT);
+        //     ctx.strokeStyle = "gray";
+        //     ctx.stroke();
+        // ////////////
         range = Math.sqrt((this.x - this.xT)**2 + (this.y - this.yT)**2) - this.radius;
         if (range <= 0 && target.canTouch) {
             //offset ball(x,y) to not go inside target if target is moving
@@ -86,7 +87,7 @@ class Ball {
         if(target.goLeft) {
             switch(this.xFlag) {
                 case 0:
-                    this.spHorizon += (this.speed - this.spHorizon)/6;
+                    this.spHorizon += (this.speed - this.spHorizon)/8;
                     break;
                 case 1:
                     this.xFlag = 0;
@@ -103,16 +104,21 @@ class Ball {
                     this.spHorizon -= this.spHorizon/3;
                     break;
                 case 1:
-                    this.spHorizon += (this.speed - this.spHorizon)/6;
+                    this.spHorizon += (this.speed - this.spHorizon)/8;
                     break;
             }
             this.spVertical = Math.sqrt(this.speed**2 - this.spHorizon**2);
             console.log("ball speed:",this.speed," /ball => vector:", this.spHorizon, "/ ball ^ vector:", this.spVertical);
         }
     }
+    spawnPower(powers,targets) {
+        powers.array.push(new Pow(targets.x,targets.y,targets.width,targets.height,"add-ball"));
+    }
+
     whenTouch(pad) {
         if(this.isTouch(pad)) {
             this.modDirection(pad);
+            pad.y = pad.yFixed;
             pad.goDown = true;
             console.log("touched");
         }
@@ -124,15 +130,17 @@ class Ball {
             pad.goUp = false;
         }
     }
-    whenTouchTargets(targets){
+    whenTouchTargets(powers, targets){
         for(let i = 0; i < targets.length; i++) {
             if(this.isTouch(targets[i]) && targets[i].canTouch) {
                 this.modDirection(targets[i]);
                 if(!targets[i].isWall) {
+                    if(Math.floor(Math.random() * 10) >= 5) {
+                        this.spawnPower(powers, targets[i]);
+                    }
                     targets[i].goDown = true;
                     targets[i].canTouch = false;
                 }
-                console.log("ok");
             }
             if (targets[i].y >= canvas.height && !targets[i].canTouch) {
                 targets[i] = 0;
@@ -149,10 +157,10 @@ class Ball {
         if(this.y - this.radius <= 0) {                 //if touch top
             this.yFlag = 1;
         }
-        if(this.y + this.radius >= canvas.height) {     //if touch bottom
-            this.touchBorderBottom = 1;
-            this.yFlag = 0;
-        }
+        // if(this.y + this.radius >= canvas.height) {     //if touch bottom
+        //     this.touchBorderBottom = 1;
+        //     this.yFlag = 0;
+        // }
     }
     move(){
         if(this.xFlag == 1) {
@@ -174,13 +182,50 @@ class Ball {
     }
 }
 
+class Balls {
+    constructor() {    
+        this.array = [];
+        this.isPlaying = true;
+    }
+    isEnd() {
+        let count = 0;
+        for(let i = 0; i < this.array.length; i++) {
+            if(typeof this.array[i] == "object") {
+                count += 1;
+            }
+        }
+        if(count == 0) {
+            this.isPlaying = false;
+        }
+    }
+    display(pad,targets,powers) {
+        this.isEnd();
+        for(let i = 0; i<this.array.length; i++) {
+            if(this.array[i]) {
+                switch(this.array[i].isHasPower) {
+                    case "no-power":
+                        this.array[i].whenTouch(pad);
+                        this.array[i].whenTouchTargets(powers,targets.array);
+                        this.array[i].whenTouchBorder();
+                        break;
+                }
+                this.array[i].move();
+                if(this.array[i].y >= canvas.height) {
+                    this.array[i] = 0;
+                    console.log(this.array);
+                }
+            }
+        }
+    }
+}
+
 class Pad {
     constructor (width) {
         this.width = width;
         this.height = 10;
         this.yFixed = canvas.height - 40;
         this.spVertical = 2;
-        this.spHorizon = 4;
+        this.spHorizon = 6;
         this.canTouch = true;
         this.goLeft = false;
         this.goRight = false;
@@ -225,9 +270,7 @@ class Tar{
     display() {
         ctx.beginPath();
         if(!this.isWall){
-            if (!this.canTouch) {
-                ctx.fillStyle = "red"
-            } else ctx.fillStyle = "green";
+            ctx.fillStyle = "green";
             ctx.strokeStyle = "white";
         } else {
             ctx.fillStyle = "gray";
@@ -238,7 +281,7 @@ class Tar{
     }
     move() {
         if(this.goDown) {
-            this.spVertical += 0.1;
+            this.spVertical += 0.3;
             this.y += this.spVertical;
         }
     }
@@ -248,7 +291,7 @@ class Targets {
     constructor() {
         this.array = [];
         this.yEachRow = 0;
-        this.tarHeight = 25;
+        this.tarHeight = 15;
     }
     setMaxInRow(number, yPos, checkPoint1, checkPoint2) {
         let x = 0;
@@ -284,32 +327,114 @@ class Targets {
     }
 }
 
-
-class BouncingBall {
-    constructor (ballRadius,padWidth) {
-        this.ball = new Ball(ballRadius);
-        this.pad = new Pad(padWidth);
-        this.targets = new Targets();
+class Pow {
+    constructor(x, y, width, height, power) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.spVertical = 2;
+        this.xT;        //coordinates of nearest touch point on target
+        this.yT;        //
+        this.power = power;
     }
-    display() {
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-        this.targets.display();
-        this.ball.whenTouch(this.pad);
-        this.ball.whenTouchTargets(this.targets.array);
-        this.ball.whenTouchBorder();
-        this.pad.move();
-        this.ball.move();
+    isTouch(target) {
+        let distantX = Math.abs(this.x+this.width/2 - (target.x+target.width/2));
+        let distantY = Math.abs(this.y+this.height/2 - (target.y+target.height/2));
+        if((this.width + target.width)/2 >= distantX && (this.height + target.height)/2 >= distantY) {
+            return true;
+        } else return false;
+    }
+    powerSelect(balls){
+        switch(this.power) {
+            case "add-ball":
+                let bal = new Bal(6, "no-power");
+                let x,y;
+                for(let i=0; i<balls.array.length ; i++) {
+                    if(typeof balls.array[i] == "object"){
+                        if(balls.array[i].spHorizon > balls.array[i].spVertical) {
+                            bal.spHorizon = balls.array[i].spHorizon/2;
+                            bal.spVertical = Math.sqrt(balls.array[i].speed**2 - bal.spHorizon**2);
+                        } else {
+                            bal.spVertical = balls.array[i].spVertical/2;
+                            bal.spHorizon = Math.sqrt(balls.array[i].speed**2 - bal.spVertical**2);
+                        }
+                        bal.xFlag = balls.array[i].xFlag;
+                        bal.yFlag = balls.array[i].yFlag;
+                        x = balls.array[i].x; 
+                        y = balls.array[i].y;
+                        break;
+                    } 
+                }
+                balls.array.push(bal);
+                bal.getCoordinates(x,y);
+                break;
+        }
+    }
+    move() {
+        ctx.beginPath();
+        ctx.fillStyle = "blue";
+        ctx.strokeStyle = "white";
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+        ctx.fillRect(this.x,this.y,this.width,this.height);
+        this.y += this.spVertical;
     }
 }
 
-let game = new BouncingBall(8,80);
+class Powers {
+    constructor(){
+        this.array = [];
+    }
+    display(pad, balls) {
+        for(let i = 0; i< this.array.length; i++){
+            if(this.array[i]) {
+                this.array[i].move();
+                if(this.array[i].isTouch(pad)){
+                    this.array[i].powerSelect(balls);
+                    this.array[i] = 0;
+                } else if(this.array[i].y >= canvas.height) {
+                    this.array[i] = 0;
+                }
+            }            
+        }
+    }
+}
+
+
+class BouncingBall {
+    constructor (padWidth) {
+        this.balls = new Balls();
+        this.pad = new Pad(padWidth);
+        this.targets = new Targets();
+        this.powers = new Powers();
+        this.isWin = false;
+    }
+    display() {
+        if(this.balls.isPlaying){
+            ctx.clearRect(0,0,canvas.width,canvas.height);
+            this.targets.display(this.powers);
+            this.pad.move();
+            this.balls.display(this.pad,this.targets,this.powers);
+            this.powers.display(this.pad,this.balls);
+        }
+    }
+}
+
+
+
+
+
+let game = new BouncingBall(80);
+game.balls.array.push(new Bal(6, "no-power"));
 game.pad.getCoordinates(canvas.width/2-game.pad.width/2);
-game.ball.getCoordinates(canvas.width/2,game.pad.y-game.ball.radius);
-game.targets.setRows(20,15);
+game.balls.array[0].getCoordinates(canvas.width/2,game.pad.y-game.balls.array[0].radius);
+game.targets.setRows(30,15);
 
 function animate(){
-    game.display();
-    requestAnimationFrame(animate);
+    setTimeout(() => {
+        game.display();
+        requestAnimationFrame(animate);
+    }, 1000/90);
 }
 animate();
 
